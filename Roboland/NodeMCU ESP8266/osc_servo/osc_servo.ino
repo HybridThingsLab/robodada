853b691b7d6c1a4236931b6cca0da9e2e3a8cfec
Elias Naphausen <>
2020-03-10 17:44:43 +0100

    control servos with nodemcu

--- Roboland/NodeMCU ESP8266/osc_servo/osc_servo.ino
+++ Roboland/NodeMCU ESP8266/osc_servo/osc_servo.ino
@@ -2,6 +2,7 @@
 
 // libraries
 #include <ArduinoOSC.h> // https://github.com/hideakitai/ArduinoOSC
+#include <Servo.h>
 
 // WiFi stuff
 const char* ssid = "maschinenraum";
@@ -9,6 +10,8 @@ const char* pwd = "maschinenraum";
 const IPAddress ip(192, 168, 1, 200);
 const IPAddress gateway(192, 168, 1, 1);
 const IPAddress subnet(255, 255, 255, 0);
+Servo s1;
+Servo s2;
 
 // for ArduinoOSC
 OscWiFi osc;
@@ -16,12 +19,18 @@ const char* host = "192.168.1.100";
 const int recv_port = 9999;
 const int send_port = 8888;
 
+uint8_t servo1Pin = D1;
+uint8_t servo2Pin = D2;
+
 
 void setup() {
   Serial.begin(9600);
 
   // pin modes
   pinMode(LED_BUILTIN, OUTPUT);
+  analogWriteFreq(1);
+  s1.attach(servo1Pin);
+  s2.attach(servo2Pin);
 
   // WiFi stuff
   WiFi.begin(ssid, pwd);
@@ -36,16 +45,29 @@ void setup() {
   osc.begin(recv_port);
 
 
-  osc.subscribe("/led", [](OscMessage & m) {
-    Serial.print("led : ");
+  osc.subscribe("/servo1", [](OscMessage & m) {
+    Serial.print("servo1 : ");
     Serial.print(m.ip()); Serial.print(" ");
     Serial.print(m.port()); Serial.print(" ");
     Serial.print(m.size()); Serial.print(" ");
     Serial.print(m.address()); Serial.print(" ");
     Serial.println(m.arg<int>(0));
 
-    int state = m.arg<int>(0);
-    controlLED(state);
+    int pos = m.arg<int>(0);
+    controlServo1(pos);
+
+  });
+
+  osc.subscribe("/servo2", [](OscMessage & m) {
+    Serial.print("servo2 : ");
+    Serial.print(m.ip()); Serial.print(" ");
+    Serial.print(m.port()); Serial.print(" ");
+    Serial.print(m.size()); Serial.print(" ");
+    Serial.print(m.address()); Serial.print(" ");
+    Serial.println(m.arg<int>(0));
+
+    int pos = m.arg<int>(0);
+    controlServo2(pos);
 
   });
 
@@ -56,7 +78,14 @@ void loop() {
 
 }
 
-void controlLED(int state) {
-  
-  Serial.println(state);
+void controlServo1(int pos) {
+  s1.write(pos);
+  Serial.print("Servo 1: ");
+  Serial.println(pos);
+}
+
+void controlServo2(int pos) {
+  s2.write(pos);
+  Serial.print("Servo 2: ");
+  Serial.println(pos);
 }
