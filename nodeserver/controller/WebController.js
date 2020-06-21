c700680f3678c2a81f5eaa3202e5b3d20afd9adb
Andreas Muxel <login@andreasmuxel.com>
2020-06-21 16:07:19 +0200

    webPort is now 3000 again

--- nodeserver/controller/WebController.js
+++ nodeserver/controller/WebController.js
@@ -2,13 +2,13 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-//var webPort = 3567;
+//var webPort = 3000;
 const path = require('path');
 
 module.exports = class WebController {
     constructor() {
         let self = this;
-        var webPort = process.env.PORT || 3000;
+        let webPort = process.env.PORT || 3000;
         server.listen(webPort, function () {
             console.log("Listening on " + webPort + " in " + __dirname);
         });
