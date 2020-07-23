59fccdb8f861b08f15f87551072af5f6c5d3d187
Andreas Muxel <login@andreasmuxel.com>
2020-07-23 17:24:59 +0200

    add config file robot again

---
+++ Roboland/NodeMCU_ESP8266/osc_servo/config.h
@@ -0,0 +1,9 @@
+#define SSID_NAME "name of your WiFi"
+#define PWD "passowrt of your WiFi"
+#define IP {192, 168, 0, 123} // ip of your robot 
+#define GATEWAY {192, 168, 0, 1} // ip of your router
+#define SUBNET {255, 255, 255, 0} // subnet of your WiFi, probably you don't have to change
+
+#define HOST "192, 168, 0, 21" // ip of the machine running ROBODADA Server
+#define RECV_PORT 9999 // receive port OSC
+#define SEND_PORT 9998 // send port
