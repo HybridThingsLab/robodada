9033c235e774010514bc6741fb8193c8c9eb4c4e
Andreas Muxel <login@andreasmuxel.com>
2020-07-23 21:39:49 +0200

    Update config.h

--- Roboland/NodeMCU_ESP8266/osc_servo/config.h
+++ Roboland/NodeMCU_ESP8266/osc_servo/config.h
@@ -1,5 +1,5 @@
 #define SSID_NAME "name of your WiFi"
-#define PWD "passowrt of your WiFi"
+#define PWD "password of your WiFi"
 #define IP {192, 168, 0, 123} // ip of your robot 
 #define GATEWAY {192, 168, 0, 1} // ip of your router
 #define SUBNET {255, 255, 255, 0} // subnet of your WiFi, probably you don't have to change
