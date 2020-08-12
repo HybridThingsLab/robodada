157d8e0ccd455924a02e64d6c7a7b6f777802671
Elias Naphausen <>
2020-08-12 11:31:32 +0200

    graceful wifi connection with minimal config

--- Roboland/NodeMCU_ESP8266/osc_servo/osc_servo.ino
+++ Roboland/NodeMCU_ESP8266/osc_servo/osc_servo.ino
@@ -7,15 +7,9 @@
 #include <ArduinoOSC.h> // https://github.com/hideakitai/ArduinoOSC
 #include <Servo.h>
 
-// Robot's properties
-const String name = "Bender";
-
 // WIFI stuff
 const char* ssid = SSID_NAME;
 const char* pwd = PWD;
-const IPAddress ip(IP);
-const IPAddress gateway(GATEWAY);
-const IPAddress subnet(SUBNET);
 
 // ArduinoOSC
 const char* host = HOST;
@@ -38,53 +32,34 @@ void setup() {
   s1.attach(servo1Pin);
   s2.attach(servo2Pin);
 
-  // WiFi stuff
+  // graceful WiFi connection
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(500);
+
   WiFi.begin(ssid, pwd);
-  WiFi.config(ip, gateway, subnet);
+  
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(500);
   }
+
   Serial.print("WiFi connected, IP = "); Serial.println(WiFi.localIP());
     
   delay(1000);
 
   OscWiFi.subscribe(recv_port, "/position", [](OscMessage & m) {
-
-
-    /* Serial.print("PosMsg : ");
-
-    Serial.print(m.remoteIP()); Serial.print(" ");
-    Serial.print(m.remotePort()); Serial.print(" ");
-    Serial.print(m.size()); Serial.print(" ");
-    Serial.print(m.address()); Serial.print(" ");
-    Serial.print(m.arg<float>(0)); Serial.print(" ");
-    Serial.println(m.arg<float>(1)); */
-
-
     float x = m.arg<float>(0);
     float y = m.arg<float>(1);
-
-    /*
-      Serial.print("x: ");
-      Serial.println(x);
-      Serial.print("y: ");
-      Serial.println(y);
-    */
-
     positionControl(x, y);
-
   });
 
   OscWiFi.subscribe(recv_port,"/hello", [](OscMessage & m) {
     Serial.print(m.remoteIP()); Serial.print(" ");
     Serial.print(m.remotePort()); Serial.print(" ");
     Serial.print(m.size()); Serial.print(" ");
-    Serial.print(m.address()); Serial.print(" ");   
-  
+    Serial.print(m.address()); Serial.print(" ");  
   });
-  
-
 }
 
 void loop() {
@@ -101,11 +76,4 @@ void positionControl(float x, float y) {
   int m_y = float_map(y, 0, 1, 0, 170);
   s1.write(m_x);
   s2.write(m_y);
-
-  /*
-    Serial.print("x: ");
-    Serial.print(m_x);
-    Serial.print(" y: ");
-    Serial.println(m_y);
-  */
 }
