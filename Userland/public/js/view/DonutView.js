68388b53110e8c97c6329b4a746fe9ba6884be83
Andreas Kraus <andreas.kraus@hs-augsburg.de>
2020-08-12 12:52:35 +0200

    Added p5 donuts

--- Userland/public/js/view/DonutView.js
+++ Userland/public/js/view/DonutView.js
@@ -1,21 +1,76 @@
+var emotionValues = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+
 class DonutView {
     constructor(){
-        this.donuts = document.querySelectorAll('.inner-donut-wrapper');
+        this.donutViewContainer = document.getElementById("donut-view");
+        this.donutRenderer = this.buildDonutRenderer();
+        this.donutView = new p5(this.donutRenderer, this.donutViewContainer);
     }
 
     handleNewTrackResult(trackresult){
-        this.donuts.forEach((elem)=>{
-            let srcString = elem.firstElementChild.src;
-            srcString = srcString.substring(srcString.lastIndexOf('/') + 1, srcString.lastIndexOf('.'));
-            //console.log(srcString, trackresult['expressions'][srcString]);
-            let donut = elem.lastElementChild;
-            if(donut.contentDocument != undefined){
-                let circles = donut.contentDocument.getElementsByClassName('circle-chart__circle');
-                circles[0].style.strokeDasharray = "" + trackresult['expressions'][srcString] * 100 +" 100";                
-                circles[1].style.strokeDasharray = "" + trackresult['expressions'][srcString] * 100 +" 100";
-            }
-            
-        })
+        emotionValues = trackresult.expressions;
     }
 
+    buildDonutRenderer(){
+        return (function(p){
+            let width = window.innerHeight;
+            let height = window.innerHeight * 0.15;
+            let strokeWeight = 6;
+            let donutScale = 0.9;
+            let innerDonutScale = 0.7;
+            let emojiScale = 1.6;
+            let emojiNames = ["angry", "disgusted", "fearful", "happy", "neutral", "sad", "surprised"];
+            let emojiPath = "img/emotions/";
+            let graphColor = "#A2D2F9";
+            let emojiImgs = [];
+
+            p.preload = function(){
+                for (let i = 0; i < emojiNames.length; i++) {
+                     emojiImgs[i] = p.loadImage(emojiPath + emojiNames[i] + ".svg");
+                }
+            }
+
+            p.setup = function(){
+                p.createCanvas(width, height);
+                p.smooth(8);
+                p.imageMode(p.CENTER);
+            }
+
+            p.draw = function(){
+                p.background("#FFFFFF");
+                for (let i = 0; i < 7; i++) {
+                    drawDonut((p.height - strokeWeight) / 2 + (p.width / 7) * i, p.height/2, i, emotionValues[emojiNames[i]]);
+                }
+            }
+
+            let drawDonut = function(x, y, emotion, val){
+                let donutSize = p.height * donutScale - strokeWeight;
+                let innerDonutSize = p.height * donutScale * innerDonutScale - strokeWeight;
+                p.noFill();
+                p.stroke(0);
+                p.strokeWeight(strokeWeight);
+                //Draw Emoji
+                let emoji = emojiImgs[emotion]
+                p.image(emoji, x, y, innerDonutSize * emojiScale, innerDonutSize * emojiScale);
+                //Prevent val == 0
+                p.strokeWeight((donutSize - innerDonutSize) / 2);
+                if(val < 0.001){
+                    val = 0.001;
+                }
+                //Draw circle
+                p.arc(x,y, (donutSize + innerDonutSize) / 2, (donutSize + innerDonutSize) / 2, -p.PI / 2, -p.PI / 2 + (val * 2 * p.PI));
+                p.stroke(graphColor);
+                p.strokeWeight((donutSize - innerDonutSize) / 3);
+                p.arc(x,y, (donutSize + innerDonutSize) / 2, (donutSize + innerDonutSize) / 2, -p.PI / 2, -p.PI / 2 + (val * 2 * p.PI));
+                
+                //Reset Stroke
+                p.stroke(0);
+                p.strokeWeight(strokeWeight);
+                //Draw outer border
+                p.ellipse(x, y, donutSize, donutSize);
+                //Draw inner border
+                p.ellipse(x, y, innerDonutSize, innerDonutSize);
+            }
+        })
+    }
 }
