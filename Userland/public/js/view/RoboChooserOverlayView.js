1713bf9e10f31bf9869703e15d815d97540adbdc
raetrakt <fabian.pitzer@gmx.de>
2020-09-07 23:16:13 +0200

    css styling: robo chooser and subtle drop shadow

--- Userland/public/js/view/RoboChooserOverlayView.js
+++ Userland/public/js/view/RoboChooserOverlayView.js
@@ -24,14 +24,6 @@ class RoboChooserOverlayView{
         })
     }
 
-    /*
-        <div class="robo-chooser-button button">
-            <img class="robo-icon" src="img/menu/robot.svg" alt="">
-            <p class="robo-name">bender</p>
-        </div>
-    */
-
-    //TODO also call this when available robots list has changed
     updateOverlayContent(roboList, currentRobo){
         this._interfaceWrapper.innerHTML = "";
         roboList.forEach(robo => {
@@ -42,11 +34,26 @@ class RoboChooserOverlayView{
             let name = document.createElement("p");
             name.classList.add("robo-name");
             name.innerText = robo.name;
-
+            
             let claimButton = document.createElement("div");
             claimButton.classList.add("claim-button");
             claimButton.classList.add("button");
             claimButton.style.display = "none";
+            
+            let waveButton = document.createElement("div");
+            waveButton.classList.add("wave-button");
+            waveButton.classList.add("button");
+            waveButton.style.display = "none";
+            
+            let waveEmoji = document.createElement("img");
+            waveEmoji.src = "img/roboChooser/wave.svg"
+            waveEmoji.classList.add("wave-emoji");
+            waveButton.appendChild(waveEmoji);
+            
+            let buttonWrapper = document.createElement("div");
+            buttonWrapper.classList.add("button-wrapper");
+            buttonWrapper.appendChild(claimButton);
+            buttonWrapper.appendChild(waveButton);
 
             let infoText = document.createElement("p");
             infoText.classList.add("info-text");
@@ -70,6 +77,7 @@ class RoboChooserOverlayView{
                 });
             } else {
                 icon.src = "img/roboChooser/connected_other.svg"
+                waveButton.style.display = "none";
                 infoText.innerText = robo.name + " is in use by someone else.";
                 claimButton.innerText = "connect anyway";
                 claimButton.addEventListener('click', () => {
@@ -81,19 +89,13 @@ class RoboChooserOverlayView{
             
             
             
-            let waveButton = document.createElement("div");
-            waveButton.classList.add("wave-button");
-            waveButton.classList.add("button");
-            waveButton.style.display = "none";
-            
             let button = document.createElement("div");
             button.classList.add("robo-chooser-button");
             button.classList.add("button");
             button.appendChild(icon);
             button.appendChild(name);
             button.appendChild(infoText);
-            button.appendChild(claimButton);
-            button.appendChild(waveButton);
+            button.appendChild(buttonWrapper);
             button.addEventListener('click', () => {
                 this._setCurrentAsActive(button);
             })
@@ -105,19 +107,28 @@ class RoboChooserOverlayView{
     _setCurrentAsActive(clickedButton) {
         let buttonList = document.querySelectorAll(".robo-chooser-button");
         buttonList.forEach(button => {
-            button.classList.remove("active");
-            button.childNodes[1].style.display = "block";
-            button.childNodes[2].style.display = "none";
-            button.childNodes[3].style.display = "none";
-            button.childNodes[4].style.display = "none";
+            if (button != clickedButton) {
+
+                button.classList.remove("active");
+                button.childNodes[1].style.display = "block";
+                button.childNodes[2].style.display = "none";
+                button.childNodes[3].style.display = "none";
+            }
         });
         clickedButton.classList.add("active");
         clickedButton.childNodes[1].style.display = "none";
         setTimeout(() => {
             clickedButton.childNodes[2].style.display = "block";
-            clickedButton.childNodes[3].style.display = "block";
-            clickedButton.childNodes[4].style.display = "block";
-        }, 125);
+            clickedButton.childNodes[3].style.display = "flex";
+            //not very elegant, has to be changed when button text is changed
+            if (clickedButton.childNodes[3].childNodes[0].innerText == "connect anyway") {
+                clickedButton.childNodes[3].childNodes[0].style.display = "inline-block";
+                clickedButton.childNodes[3].childNodes[1].style.display = "none";
+            } else {
+                clickedButton.childNodes[3].childNodes[0].style.display = "inline-block";
+                clickedButton.childNodes[3].childNodes[1].style.display = "inline-block";
+            }
+        }, 110);
 
     }
 
