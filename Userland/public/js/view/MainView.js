efd6032ef80a218642bd7bab097587c8cd4cc1f2
Andreas Muxel <login@andreasmuxel.com>
2020-09-14 15:46:55 +0200

    no changes, just auto-format

--- Userland/public/js/view/MainView.js
+++ Userland/public/js/view/MainView.js
@@ -1,20 +1,20 @@
 class MainView {
-    constructor(emotionModel){
+    constructor(emotionModel) {
         document.getElementsByClassName("cam-video")[0].addEventListener("loadedmetadata", this._onPlay);
-        document.addEventListener('keydown', (key)=>{
-            if(key.code == "F2"){
+        document.addEventListener('keydown', (key) => {
+            if (key.code == "F2") {
                 dispatchEvent(new CustomEvent("notifyOpenDevVisualization"));
             }
-            if(key.code == "KeyS"){
+            if (key.code == "KeyS") {
                 dispatchEvent(new CustomEvent("notifySaveToJSONFile"));
             }
-            if(key.code == "KeyL"){
+            if (key.code == "KeyL") {
                 dispatchEvent(new CustomEvent("notifyLoadFromJSONFile"));
             }
         });
         this.emoticons = {};
-        for(let emo in EmotionEnum) {
-            console.log(emo + " " +  EmotionEnum[emo]);
+        for (let emo in EmotionEnum) {
+            console.log(emo + " " + EmotionEnum[emo]);
             this.emoticons[EmotionEnum[emo]] = new Image();
             this.emoticons[EmotionEnum[emo]].src = emotionModel.getEmotionObject(emo).svg;
         }
@@ -22,7 +22,9 @@ class MainView {
 
     saveJSON(content, fileName) {
         var a = document.createElement("a");
-        var file = new Blob([JSON.stringify(content)], {type: 'text/plain'});
+        var file = new Blob([JSON.stringify(content)], {
+            type: 'text/plain'
+        });
         a.href = URL.createObjectURL(file);
         a.download = fileName;
         a.click();
@@ -33,77 +35,81 @@ class MainView {
         input.type = 'file';
         input.click();
 
-        input.onchange = e =>{
+        input.onchange = e => {
             let file = e.target.files[0];
             if (!file) {
                 return;
             }
             let reader = new FileReader();
-            reader.onload = function(e) {
+            reader.onload = function (e) {
                 let contents = e.target.result;
                 let jsonContent = JSON.parse(contents);
-                dispatchEvent(new CustomEvent("notifyLoadedJSONFile", {detail: jsonContent}));
+                dispatchEvent(new CustomEvent("notifyLoadedJSONFile", {
+                    detail: jsonContent
+                }));
             };
             reader.readAsText(file);
         }
     }
 
 
-    _onPlay(e){        
-        let event = new CustomEvent("notifyVideoPlay", {detail: e.srcElement});
+    _onPlay(e) {
+        let event = new CustomEvent("notifyVideoPlay", {
+            detail: e.srcElement
+        });
         dispatchEvent(event);
     }
 
-    
+
     /**
      * @description updates the emojiposition with a new tracking result
      * @param {tracking result object} trackingResult 
      */
-    updateEmojiPosition(trackingResult){        
+    updateEmojiPosition(trackingResult) {
         let canvas = document.getElementsByClassName("emoji-canvas")[0];
         let dims = faceapi.matchDimensions(canvas, document.getElementsByClassName("cam-video")[0], true);
         let resizedResult = faceapi.resizeResults(trackingResult.tracking, dims);
-        
+
         let ctx = canvas.getContext('2d');
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        let scale = resizedResult.detection.box.width/canvas.width*8;
-        ctx.setTransform(scale, 0, 0, scale, resizedResult.detection.box.x+resizedResult.detection.box.width/2, resizedResult.detection.box.y+resizedResult.detection.box.height / 2);    
-        
+        let scale = resizedResult.detection.box.width / canvas.width * 8;
+        ctx.setTransform(scale, 0, 0, scale, resizedResult.detection.box.x + resizedResult.detection.box.width / 2, resizedResult.detection.box.y + resizedResult.detection.box.height / 2);
+
         //@TODO: manage emoticons       
-        ctx.drawImage(this.emoticons[trackingResult.emotion],-100 , -120 );
+        ctx.drawImage(this.emoticons[trackingResult.emotion], -100, -120);
     }
-    
+
     /**
      * @description translates the main menu so you see the live webcam feed 
      */
-    switchToPlaybackView(){
+    switchToPlaybackView() {
         console.log("switching to playback view");
         let cam_button = document.querySelector(".camera-button");
         document.getElementsByClassName("main-screen")[0].style.transform = "translate(0, -86vh)";
         cam_button.firstElementChild.innerHTML = "<p>deactivate&nbsp;cam</p>";
         cam_button.lastElementChild.src = "img/menu/camera_off.svg";
     }
-    
+
     /**
      * @description translates the webcam emotion detection area so you see the planning or emotion drawing area
      */
-    switchToPlanningView(){
+    switchToPlanningView() {
         console.log("switching to planning view");
         let cam_button = document.querySelector(".camera-button");
         document.getElementsByClassName("main-screen")[0].style.transform = "";
         cam_button.firstElementChild.innerHTML = "<p>activate&nbsp;cam</p>";
         cam_button.lastElementChild.src = "img/menu/camera_on.svg";
     }
-    
+
     /**
      * @description returns dom element for cam video
      * @returns {dom element} camera video element
      */
-    getVideoElement(){
+    getVideoElement() {
         return document.getElementsByClassName("cam-video")[0];
     }
 
-    
 
-    
+
+
 }
