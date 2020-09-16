a0566acb6bc0895f73fa951039d6ea2886793427
Andreas Muxel <login@andreasmuxel.com>
2020-09-17 00:05:49 +0200

    Update config.h
    re-format config.h

--- Roboland/NodeMCU_ESP8266/osc_servo/config.h
+++ Roboland/NodeMCU_ESP8266/osc_servo/config.h
@@ -1,5 +1,7 @@
-#define RECV_PORT 9999 // receive port OSC
-#define SEND_PORT 9998 // send port
 #define ROBO_NAME "Bender"
+
 #define SSID_NAME "name of your WiFi"
 #define PWD "password of your WiFi"
+
+#define RECV_PORT 9999 // receive port OSC
+#define SEND_PORT 9998 // send port
