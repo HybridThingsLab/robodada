83c778d688636a7128d009db48e32ec0472a5e11
Elias Naphausen <>
2022-08-09 16:22:53 +0200

    establish socketio connections via the certified url

--- Serverland/config.js
+++ Serverland/config.js
@@ -2,7 +2,8 @@ config = {
     robot_ip: "192.168.188.36",
     frontend_location_relative: "./../../Userland/public",
     privateSSLKey_location_relative: "./cert/privkey.pem",
-    publicCert_location_relative: "./cert/fullchain.pem"
+    publicCert_location_relative: "./cert/fullchain.pem",
+    certifiedURL: "https://robodada.exampleurl.com"
 }
 
 module.exports = config;
