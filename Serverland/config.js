4d2e42cdde233daeeaca4f8bd0c2cfdbb755b6c3
Elias Naphausen <>
2022-08-09 16:15:35 +0200

    start server with https

--- Serverland/config.js
+++ Serverland/config.js
@@ -1,6 +1,8 @@
 config = {
     robot_ip: "192.168.188.36",
-    frontend_location_relative: "./../../Userland/public"
+    frontend_location_relative: "./../../Userland/public",
+    privateSSLKey_location_relative: "./cert/privkey.pem",
+    publicCert_location_relative: "./cert/fullchain.pem"
 }
 
 module.exports = config;
