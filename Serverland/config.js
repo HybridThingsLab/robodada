8fbf31904d2ad69df9dd2acc2ba1d2193c5ecf39
Elias Naphausen <elias.naphausen@hs-augsburg.de>
2024-07-30 14:15:30 +0200

    set correct certificate paths

--- Serverland/config.js
+++ Serverland/config.js
@@ -1,9 +1,9 @@
 config = {
     robot_ip: "192.168.188.36",
     frontend_location_relative: "./../../Userland/public",
-    privateSSLKey_location_relative: "./cert/privkey.pem",
-    publicCert_location_relative: "./cert/fullchain.pem",
-    certifiedURL: "https://robodada.exampleurl.com"
+    privateSSLKey_location_relative: "/etc/letsencrypt/live/robots.hybridthings.com/privkey.pem",
+    publicCert_location_relative: "/etc/letsencrypt/live/robots.hybridthings.com/fullchain.pem",
+    certifiedURL: "https://robots.hybridthings.com"
 }
 
 module.exports = config;
